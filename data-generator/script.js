014e3fd37a6df9f43255f1a7fc00e76c995637ad
Wrona <87375046+WronaDEV@users.noreply.github.com>
2023-11-11 22:46:13 +0100

    Add files via upload

---
+++ data-generator/script.js
@@ -0,0 +1,27 @@
+const pl_m_name = [
+    'Jan', 'Kacper', 'Mateusz', 'Michał', 'Jakub', 'Szymon', 'Bartosz', 'Adam', 'Piotr', 'Paweł',
+    'Wiktor', 'Adrian', 'Kamil', 'Mikołaj', 'Krzysztof', 'Tomasz', 'Patryk', 'Łukasz', 'Marcin', 'Daniel',
+    'Rafał', 'Hubert', 'Grzegorz', 'Damian', 'Sebastian', 'Robert', 'Artur', 'Alan', 'Dawid', 'Oliwier',
+    'Igor', 'Filip', 'Marek', 'Dominik', 'Radosław', 'Aleksander', 'Maciej', 'Łukasz', 'Emil', 'Arkadiusz',
+    'Jacek', 'Ksawery', 'Ernest', 'Bartłomiej', 'Juliusz', 'Cezary', 'Konrad', 'Norbert', 'Ryszard', 'Łukasz',
+    'Ireneusz', 'Zenon', 'Edward', 'Henryk', 'Przemysław', 'Tadeusz', 'Władysław', 'Kazimierz', 'Artur', 'Aleks',
+    'Bogdan', 'Leszek', 'Józef', 'Janusz', 'Zbigniew', 'Dariusz', 'Ernest', 'Waldemar', 'Ryszard', 'Stanisław',
+    'Tymoteusz', 'Gerard', 'Karol', 'Leon', 'Zygmunt', 'Adrian', 'Bogusław', 'Cyprian', 'Felicjan', 'Leonard'
+];
+
+const pl_m_lname = [
+    'Nowak', 'Kowalski', 'Wiśniewski', 'Wójcik', 'Kowalczyk', 'Kamiński', 'Lewandowski', 'Zieliński', 'Szymański', 'Woźniak',
+    'Dąbrowski', 'Kozłowski', 'Jankowski', 'Mazur', 'Wojciechowski', 'Kwiatkowski', 'Kaczmarek', 'Piotrowski', 'Grabowski', 'Zając',
+    'Pawłowski', 'Michalski', 'Król', 'Wieczorek', 'Jabłoński', 'Wróbel', 'Nowakowski', 'Majewski', 'Olszewski', 'Stępień',
+    'Jaworski', 'Malinowski', 'Pawlak', 'Witkowski', 'Walczak', 'Stankiewicz', 'Baran', 'Sadowski', 'Duda', 'Gajewski',
+    'Rutkowski', 'Michalak', 'Sikora', 'Ostrowski', 'Tomaszewski', 'Zalewski', 'Wróblewski', 'Marciniak', 'Jasiński', 'Zawadzki',
+    'Sokół', 'Czerwiński', 'Błaszczyk', 'Kubiak', 'Kwiecień', 'Marcinkowski', 'Kaczmarczyk', 'Mazurek', 'Włodarczyk', 'Adamczyk',
+    'Górski', 'Jóźwiak', 'Bednarek', 'Makowski', 'Nawrocki', 'Wrona', 'Krajewski', 'Leszczyński', 'Górecki', 'Głowacki',
+    'Klimek', 'Kołodziej', 'Matuszewski', 'Wawrzyniak', 'Sobczak', 'Baranowski', 'Laskowski', 'Brzeziński', 'Czech', 'Kowalewski',
+    'Leszczynski', 'Cieślak', 'Szymczak', 'Gajda', 'Krawczyk', 'Wesołowski', 'Kołodziejczyk', 'Urbański', 'Kaźmierczak', 'Czarnecki'
+];
+
+function GenerateData() {
+    document.getElementById("data-box").style.display = "none";
+    document.getElementById("results-box").style.display = "block";
+}
